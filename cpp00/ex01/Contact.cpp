9c2eb101e8ca353e32ddf08b4932f134af902538
Cira Estrade Garcia <ciestrad@c3r1s4.42urduliz.com>
2025-02-13 13:29:34 +0100

    git

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: ciestrad <ciestrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 12:15:53 by ciestrad          #+#    #+#             */
-/*   Updated: 2025/02/11 12:17:44 by ciestrad         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:22:40 by ciestrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,50 @@
 void Contacts::setFirstName(std::string input)
 {
 	this->first_name = input;
+}
+
+void Contacts::setLastName(std::string input)
+{
+	this->last_name = input;
+}
+
+void Contacts::setNickname(std::string input)
+{
+	this->nickname = input;
+}
+void Contacts::setPhoneName(std::string input)
+{
+	this->phone_number = input;
+}
+void Contacts::setSecret(std::string input)
+{
+	this->darkest_secret = input;
+}
+
+
+
+
+const std::string& Contacts::getFirstName(void) const
+{
+	return this->first_name;
+}
+
+const std::string& Contacts::getLastName(void) const
+{
+	return this->last_name;
+}
+
+const std::string& Contacts::getNickname(void) const
+{
+	return this->nickname;
+}
+
+const std::string& Contacts::getPhoneName(void) const
+{
+	return this->phone_number;
+}
+
+const std::string& Contacts::getSecret(void) const
+{
+	return this->darkest_secret;
 }
--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -6,23 +6,76 @@
 /*   By: ciestrad <ciestrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:42:01 by ciestrad          #+#    #+#             */
-/*   Updated: 2025/02/11 13:26:14 by ciestrad         ###   ########.fr       */
+/*   Updated: 2025/02/13 11:27:51 by ciestrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+#include <stdio.h>
+
+int chack_contact(const Contacts contact)
+{
+	if(contact.getFirstName().empty() ||
+		contact.getLastName().empty() ||
+		contact.getNickname().empty() ||
+		contact.getPhoneName().empty() ||
+		contact.getSecret().empty())
+		{
+			return(0);
+			std::cout << "Contact bad stored" << std::endl;
+		}
+	else	
+		return(1);
+}
+
+PhoneBook::PhoneBook(void)
+{
+	count = 0;
+}
 
 void PhoneBook::addcontact()
 {
 	Contacts	new_contact;
 	std::string	input;
+	const std::string opcion[5] = {"First Name: ", "Last Name: ", "Nickname: ", "Phonename: ", "Darkest Secret: "};
 
-	std::cout << "Insert First Name" << std::endl;
+	
+	for(int i = 0 ; i <= 4; i++)
+	{
+		std::cout << "Insert " + opcion[i] << std::endl;
+		std::cout << ">";
+		std::getline(std::cin, input);
+		if(i == 3)
+		{
+			while(input.empty() || !is_number(input))
+			{
+				std::cout << "Invalid Phonenumber" << std::endl;
+				std::cout << "Try Again: " << std::endl;
+				std::cout << ">";
+				std::getline(std::cin, input);
+			}
+			new_contact.setFirstName(input);
+			
+		}
+	}
+	//std::cout << "venga" << new_contact.getFirstName() << std::endl;
+	if(chack_contact(new_contact) == 0)
+		return ;
+	if(count > 8)
+		this->contactos[count - 1] = new_contact;
+	else
+	{
+		this->count++;
+		this->contactos[count - 1] = new_contact;
+	}
+	
+	
+	/* std::cout << "Insert First Name" << std::endl;
 	std::getline(std::cin, input);
 	if (!is_number(input))
 		new_contact.setFirstName(input);
 	else
-		std::cout << "el nombre conio" << std::endl;
+		std::cout << "el nombre conio" << std::endl; */
 		
 	
 }
