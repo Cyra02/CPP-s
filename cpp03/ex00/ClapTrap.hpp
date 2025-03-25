1d1e4590a66c27c31f10fa8f81958ca473f40acc
Cira Estrade Garcia <ciraestrade@gmail.com>
2025-03-25 12:10:32 +0100

    prueba

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,17 +6,18 @@
 /*   By: ciestrad <ciestrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 11:54:33 by ciestrad          #+#    #+#             */
-/*   Updated: 2025/03/25 12:04:01 by ciestrad         ###   ########.fr       */
+/*   Updated: 2025/03/25 12:09:10 by ciestrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
-class CLAPTRAP
+class ClapTrap
 {
 	public:
 			ClapTrap();
+			~ClapTrap();
 	
 };
 
